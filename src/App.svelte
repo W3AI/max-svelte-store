6ebcd8865de86a53299f91f86f4ae1f60545e930
stefian <v_ianta@yahoo.com>
2019-05-22 13:00:58 -0400

    added a timer/store in App comp

--- src/App.svelte
+++ src/App.svelte
@@ -2,8 +2,13 @@
   import Cart from "./Cart/Cart.svelte";
   import Products from "./Products/Products.svelte";
   import Button from './UI/Button.svelte';
+  import { timer } from './timer-store.js';
 
   let showCart = true;
+
+  timer.subscribe(count => {
+      console.log(count);
+  });
 </script>
 
 <Button on:click={() => {showCart = !showCart}}>Toggle Cart</Button>
